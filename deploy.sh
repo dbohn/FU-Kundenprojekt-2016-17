6c153361ceb868a969ab5270d6f26859bbb17356
David Bohn <davbohn@googlemail.com>
2017-01-05 13:27:25 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+MVN_DOCKER_VERSION=3.3.9-jdk-8
+DEMONSTRATOR_DOCKER_USER=1000
+HUMHUB_DOCKER_USER=33
+COMPOSE_FILE=docker-compose.deploy.yml
+
+# Create possibly missing assets directory
+if [ ! -d ./humhub/assets ]; then
+    echo "Creating missing directory humhub/assets..."
+    mkdir -p ./humhub/assets
+    echo "[Done]\n"
+fi
+
+# Get the OS Architecture
+os=$(uname -s)
+
+# Set HumHub Directory Permissions
+if [ $os = "Linux" ]; then
+    echo "Setting directory permissions for humhub..."
+    sudo chown -R $HUMHUB_DOCKER_USER ./humhub
+    echo "[Done]\n"
+
+    echo "Setting directory permissions for maven build..."
+    sudo chown -R $HUMHUB_DOCKER_USER ./build
+    echo "[Done]\n"
+fi
+
+# Build Demonstrator
+docker run -it --rm --name build-demonstrator -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven/Demonstrator maven:$MVN_DOCKER_VERSION mvn clean package
+
+# Move built Demonstrator.war to container build
+mv build/Demonstrator.war images/wildfly_deploy/Demonstrator.war
+
+# Build containers
+docker-compose --file $COMPOSE_FILE build --force-rm
+
+# Remove .war file
+rm images/wildfly_deploy/Demonstrator.war
+
+# Start containers
+docker-compose --file $COMPOSE_FILE up -d
